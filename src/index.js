b17e77e0570fd54835f7b72af8d1237d759259cf
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-07-16 17:30:31 +0200

    Refactored app to class based component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCkkasFx90fwnyH4gd2hzxwjuU4nATagNI';
 
-//searching on yt
-//parameters & callback function
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-
 //Create a new component -> creating HTML
-const App = () => {
-    return <div>
-        <SearchBar/>
-        <br/>
-        Ble
-    </div>; //JSX
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { videos: []};
+
+        //searching on yt
+        //parameters & callback function
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            //this,setState({ videos : videos}) is alternative syntax here 
+        });
+    }
+    render() {
+        return (
+        <div>
+            <SearchBar/>
+        </div>);
+    }
+}
 
 //Take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App/>, document.querySelector('.container'));
