a313f6f891d91243a284009cff0c98615bd3798e
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-07-16 18:02:35 +0200

    Added video list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCkkasFx90fwnyH4gd2hzxwjuU4nATagNI';
 
@@ -23,6 +24,7 @@ class App extends Component {
         return (
         <div>
             <SearchBar/>
+            <VideoList videos={this.state.videos}/>
         </div>);
     }
 }
