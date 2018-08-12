92109640ecc2cf59c5df6e0d9fc6882390af79bf
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-08-12 19:12:15 +0200

    Updated video handling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCkkasFx90fwnyH4gd2hzxwjuU4nATagNI';
 
@@ -11,20 +12,29 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: []};
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
 
         //searching on yt
         //parameters & callback function
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
-            //this,setState({ videos : videos}) is alternative syntax here 
+        YTSearch({key: API_KEY, term: 'ufo'}, (videos) => {
+            //this.setState({ videos }); is alternative syntax here
+            this.setState({
+                 videos : videos,
+                 selectedVideo : videos[0]
+                });  
         });
     }
     render() {
         return (
         <div>
             <SearchBar/>
-            <VideoList videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos}/>
         </div>);
     }
 }
