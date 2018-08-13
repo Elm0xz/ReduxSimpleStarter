4e2fa552ee2ec060832d6500b64e621791b3ff36
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-08-13 19:11:18 +0200

    Added delay to refreshing ytsearch using lodash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -33,9 +34,12 @@ class App extends Component {
     }
 
     render() {
+        //adding delay to videoSearch
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 800);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={term => videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
